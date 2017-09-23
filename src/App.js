d5a8a1a459a969287239bcd2c401da7bd6603d84
viyanateaa <viyanateaa@gmail.com>
2017-09-23 16:18:42 +0200

    build login page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
-import React, { Component } from 'react';
-import Header from './Header.js';
-import {Row, Col} from 'react-bootstrap';
-import './App.css';
-import Clock from './Clock.js';
-// import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      deadline: ' 2017, Sep, 22',
-      newDeadline:''
-    }
-
-  }
-  changeDeadline(){
-    this.setState({deadline:this.state.newDeadline})
-  }
+import React from 'react';
+import HomePage from './components/pages/HomePage.js';
+import LoginPage from './components/pages/LoginPage.js';
+import {Route} from "react-router-dom";
 
 
-  render() {
-    return (
+const App = () =>(
+  <div className="ui container">
 
-            <div className="App">
-              <div className="App-title"> Countdown to {this.state.deadline}</div>
-              <Clock />
-
-
-
-
-                <input placeholder = 'new date'
-                onChange={event => this.setState({newDeadline: event.target.value})}/>
-                <button onClick={() => this.changeDeadline()}>
-                  Submit
-                </button>
-
-            </div>
-
-    );
-  }
-}
+    <Route path="/"  exact component ={HomePage} />
+    <Route path="/login"  exact component ={LoginPage} />
+  </div>
+);
 
 export default App;
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route} from "react-router-dom";
+import {Link}from "react-router-dom";
+
+const LoginPage = () =>(
+  <div>
+    <h1>login</h1>
+    <Link to="/HomePage">back</Link>
+  </div>
+
+);
+export default LoginPage;
